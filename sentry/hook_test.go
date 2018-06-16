3d35d5be745c81c1cd7c19a4b8e90ba6ebe5bd94
AbeIgnocious <k.l.kubat@gmail.com>
2018-06-16 12:47:08 +0200

    make integration tests optional

--- sentry/hook_test.go
+++ sentry/hook_test.go
@@ -4,12 +4,28 @@ import (
 	"github.com/sirupsen/logrus"
 	"reflect"
 	"testing"
+	"flag"
 )
 
-// A sentry dns made specifically for this test. Don't use please.
+// A sentry dns made specifically for this test. Don't use elsewhere please.
 var dsn = `https://f73d12f7192f4943b71f1b4ba85a8a73:488214b852684b998ff13698c0525948@sentry.io/1226371`
 
+var integration bool
+
+func init() {
+	flag.BoolVar(&integration, "int", false, "run integration tests alongside unit tests")
+	flag.Parse()
+}
+
+// Unit tests.
+
+
+
+
+
+// Integration tests.
 func TestNewSentryHook(t *testing.T) {
+	if !integration {t.Skip()}
 	_, err := NewSentryHook(dsn)
 	if err != nil {
 		t.Error("Failed to create a new sentry hook (check the dsn): ", err)
@@ -17,6 +33,7 @@ func TestNewSentryHook(t *testing.T) {
 }
 
 func TestSentryHook_Fire(t *testing.T) {
+	if !integration {t.Skip()}
 	event := logrus.Entry{}
 	client, _ := NewSentryHook(dsn)
 	err := client.Fire(&event)
@@ -26,6 +43,7 @@ func TestSentryHook_Fire(t *testing.T) {
 }
 
 func TestSentryHook_Levels(t *testing.T) {
+	if !integration {t.Skip()}
 	client, _ := NewSentryHook(dsn)
 	levels := client.Levels()
 
